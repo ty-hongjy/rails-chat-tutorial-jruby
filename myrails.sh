096c2aab05138d82467136562d779363e2c30c31
ty-hongjy <hongjy1@163.com>
2021-01-04 02:28:22 +0800

    20210106

--- myrails.sh
+++ myrails.sh
@@ -1,9 +1,2 @@
-
-#d work/gx_report
-#m work/gx_report/config/puma.rb
-rm config/puma.rb
-#cd work/gx_report
-#sudo bundle exec rails s --binding=0.0.0.0 -p=3001
 jruby -J-Xmx1024m -S bundle exec rails s --binding=0.0.0.0 -p=3001
-#uudo bundle exec rails s --binding=192.168.2.174
 
